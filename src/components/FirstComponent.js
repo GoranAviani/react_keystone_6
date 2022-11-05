d38c6487537399cc8310b13add3cb1d0bb615de1
goranaviani <goranaviani@gmail.com>
2022-11-05 16:40:06 +0100

    setup

--- src/components/FirstComponent.js
+++ src/components/FirstComponent.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { useQuery } from '@apollo/client';
 
-import {POSTS_DATA} from "../../apollo/graphql/queries/posts";
+import {POSTS_DATA} from "../apollo/graphql/queries/posts";
 
 
 const FirstComponent = () => {
   const { loading, error, data } = useQuery(POSTS_DATA);
-
+    console.log({data})
     return(
         <>
         this is first component
