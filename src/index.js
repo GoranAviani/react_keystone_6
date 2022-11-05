a2c7024e0aac448e48b2e99692ff87e084810093
goranaviani <goranaviani@gmail.com>
2022-11-05 16:34:41 +0100

    setup

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {ApolloClient, InMemoryCache, ApolloProvider, gql} from '@apollo/client';
 
+export const client = new ApolloClient({
+      uri: 'https://flyby-gateway.herokuapp.com/',
+
+    cache: new InMemoryCache(),
+    connectToDevTools: true
+});
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
