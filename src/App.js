3408550aae42713994a53a8a3c7e7f2c8feaea3a
goranaviani <goranaviani@gmail.com>
2022-11-05 16:38:09 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import FirstComponent from "./components/FirstComponent";
 
 function App() {
   return (
     <div className="App">
-
+<FirstComponent />
     </div>
   );
 }
